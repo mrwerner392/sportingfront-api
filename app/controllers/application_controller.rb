5baed9a912136093429c260d35b5f8701d47621f
mrwerner392 <mrwerner392@gmail.com>
2021-05-22 14:38:30 -0400

    Add authorized before action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
   def authentication_json(user_id)
     { token: make_token(user_id), user: User.find(user_id) }
@@ -11,6 +12,24 @@ class ApplicationController < ActionController::API
 
   def hmac_secret
     ENV["JWT_SECRET_KEY"]
+  end
+
+  def authorized
+    render json: { message: 'Please log in.'}, status: :unauthorized unless logged_in?
+  end
+
+  def logged_in?
+    !!logged_in_user_id
+  end
+
+  def logged_in_user_id
+    token = request.headers["Authorization"]
+    begin
+      decoded_payload = JWT.decode(token, hmac_secret, true, {algorithm: 'HS256'})
+      return decoded_payload.first["user_id"].to_i
+    rescue
+      return nil
+    end
   end
 
 end

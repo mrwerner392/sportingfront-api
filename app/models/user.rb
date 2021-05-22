867f0f16cf3874ae03f43d437ff1dbf79c06e33f
mrwerner392 <mrwerner392@gmail.com>
2021-05-22 13:53:50 -0400

    User validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+
 end
